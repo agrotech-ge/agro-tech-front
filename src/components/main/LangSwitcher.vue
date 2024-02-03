0368083a15da536fb1bce4dc338204f100527240
Gandobilis <lashadeveloper@gmail.com>
2024-02-03 13:36:44 +0400

    language change onClick.

--- src/components/main/LangSwitcher.vue
+++ src/components/main/LangSwitcher.vue
@@ -1,7 +1,15 @@
+<script setup>
+import {ref} from "vue";
+
+const lang = ref('ka')
+</script>
+
 <template>
   <div class="flex items-center gap-x-2 font-medium">
-    <p class="font-semibold" v-text="'ქარ'"/>
-    <span class="w-[1px]"/>
-    <p class="font-semibold" v-text="'ინგ'"/>
+    <p @click="lang = 'ka'" :class="{'text-primary':lang === 'ka'}" class="font-semibold cursor-pointer"
+       v-text="'ქარ'"/>
+    <div class="w-[1px] h-5 bg-black"/>
+    <p @click="lang = 'en'" :class="{'text-primary':lang === 'en'}" class="font-semibold cursor-pointer"
+       v-text="'ინგ'"/>
   </div>
 </template>
