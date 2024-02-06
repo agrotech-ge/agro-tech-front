0332c98710e695f18e2fcc70817ba09cda0c2163
Gandobilis <lashadeveloper@gmail.com>
2024-02-06 20:45:31 +0400

    მთავარი: წევრების სათაური.

--- src/components/main/Members.vue
+++ src/components/main/Members.vue
@@ -6,7 +6,11 @@ const {members} = useMain()
 </script>
 
 <template>
-  <div class="grid grid-cols-6 gap-x-10">
-    <member v-for="(member, index) in members" :key="index" :data="member"/>
+  <div class="flex flex-col gap-y-16">
+    <p class="text-secondary font-medium text-5xl">ჩვენი გუნდი</p>
+
+    <div class="grid grid-cols-6 gap-x-10">
+      <member v-for="(member, index) in members" :key="index" :data="member"/>
+    </div>
   </div>
 </template>
