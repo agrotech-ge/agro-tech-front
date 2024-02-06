e2e3fb3d54f49fc2930fd32997c1c43e08f4fec6
Gandobilis <lashadeveloper@gmail.com>
2024-02-06 20:39:04 +0400

    მთავარი: არწერის ანიმაცია. (განგამ style)

--- src/views/Listings.vue
+++ src/views/Listings.vue
@@ -1,5 +1,7 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    განცხადებები
-  </h1>
+  <div class="flex items-center justify-center pt-16 pb-32">
+    <h1 class="text-3xl font-bold underline">
+      განცხადებები
+    </h1>
+  </div>
 </template>
