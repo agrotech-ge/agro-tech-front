1dbd6e2c5d451cc0ad1440a8053db7db2c21ff07
Gandobilis <lashadeveloper@gmail.com>
2024-02-26 15:56:00 +0400

    code structure refactored.

--- src/composables/useMain.js
+++ src/composables/useMain.js
@@ -3,49 +3,49 @@ import {ref} from "vue"
 export default function useMain() {
     const tractors = ref([
         {
-            photo: '/src/assets/images/tractors/tractor1.svg',
+            photo: '/assets/images/tractors/tractor1.svg',
             publishDate: '2024-02-03 08:30',
             title: 'ქირავდება ტრაქტორი Kubota MU4501, არის იდეალურ მდ...',
             price: '10,000 ₾'
         },
         {
-            photo: '/src/assets/images/tractors/tractor2.svg',
+            photo: '/assets/images/tractors/tractor2.svg',
             publishDate: '2024-02-03 09:45',
             title: 'იყიდება ტრაქტორი Powertrac Euro...',
             price: '12,000 ₾'
         },
         {
-            photo: '/src/assets/images/tractors/tractor3.svg',
+            photo: '/assets/images/tractors/tractor3.svg',
             publishDate: '2024-02-03 11:00',
             title: 'ქირავდება',
             price: '15,000 ₾'
         },
         {
-            photo: '/src/assets/images/tractors/tractor4.svg',
+            photo: '/assets/images/tractors/tractor4.svg',
             publishDate: '2024-02-03 13:15',
             title: 'იყიდება',
             price: '20,000 ₾'
         },
         {
-            photo: '/src/assets/images/tractors/tractor5.svg',
+            photo: '/assets/images/tractors/tractor5.svg',
             publishDate: '2024-02-03 14:30',
             title: 'იყიდება John Deere 3028EN, საუკეთესო მდგომარებაში...',
             price: '25,000 ₾'
         },
         {
-            photo: '/src/assets/images/tractors/tractor6.svg',
+            photo: '/assets/images/tractors/tractor6.svg',
             publishDate: '2024-02-03 16:45',
             title: 'ქირავდება John Deere 3028EN, საუკეთ...',
             price: '30,000 ₾'
         },
         {
-            photo: '/src/assets/images/tractors/tractor7.svg',
+            photo: '/assets/images/tractors/tractor7.svg',
             publishDate: '2024-02-03 18:00',
             title: 'იყიდება',
             price: '35,000 ₾'
         },
         {
-            photo: '/src/assets/images/tractors/tractor8.svg',
+            photo: '/assets/images/tractors/tractor8.svg',
             publishDate: '2024-02-03 19:15',
             title: 'ქირავდება, სასწრაფოდ',
             price: '40,000 ₾'
@@ -54,32 +54,32 @@ export default function useMain() {
 
     const members = ref([
         {
-            photo: '/src/assets/images/members/member1.svg',
+            photo: '/assets/images/members/member1.svg',
             name: 'ნიკოლოზ კაციტაძე',
             description: 'ჯგუფის ლიდერი, მენტორი, იდეის ავტორი'
         },
         {
-            photo: '/src/assets/images/members/member2.svg',
+            photo: '/assets/images/members/member2.svg',
             name: 'ლაშა გაგნიძე',
             description: 'front-end დეველოპერი, back-end დეველოპერი'
         },
         {
-            photo: '/src/assets/images/members/member3.svg',
+            photo: '/assets/images/members/member3.svg',
             name: 'კარლო ცუცქირიძე',
             description: 'front-end დეველოპერი, back-end დეველოპერი'
         },
         {
-            photo: '/src/assets/images/members/member4.svg',
+            photo: '/assets/images/members/member4.svg',
             name: 'სანდრო ღუღუნიშვილი',
             description: 'back-end დეველოპერი'
         },
         {
-            photo: '/src/assets/images/members/member5.svg',
+            photo: '/assets/images/members/member5.svg',
             name: 'ვახო ბარათაშვილი',
             description: 'back-end დეველოპერი, UI/UX დიზაინერი'
         },
         {
-            photo: '/src/assets/images/members/member6.svg',
+            photo: '/assets/images/members/member6.svg',
             name: 'ანდრია ბიბიაშვილი',
             description: 'back-end დეველოპერი'
         }
@@ -87,32 +87,32 @@ export default function useMain() {
 
     const _members = ref([
         {
-            photo: '/src/assets/images/members/member1.svg',
+            photo: '/assets/images/members/member1.svg',
             name: 'ნიკოლოზ კაციტაძე',
             description: 'ნიკოლოზ კაციტაძე არის ჯგუფის ლიდერი, მენტორი და იდეის ავტორი.'
         },
         {
-            photo: '/src/assets/images/members/member2.svg',
+            photo: '/assets/images/members/member2.svg',
             name: 'ლაშა გაგნიძე',
             description: 'ლაშა გაგნიძე არის ჯგუფის წევრი, რომელსაც ეკუთვნის ამ  საიტის front და back მხარეები.'
         },
         {
-            photo: '/src/assets/images/members/member3.svg',
+            photo: '/assets/images/members/member3.svg',
             name: 'კარლო ცუცქირიძე',
             description: 'კარლო ცუცქირიძე არის ჯგუფის წევრი, რომელსაც ეკუთვნის ამ საიტის front და back მხარეები.'
         },
         {
-            photo: '/src/assets/images/members/member4.svg',
+            photo: '/assets/images/members/member4.svg',
             name: 'სანდრო ღუღუნიშვილი',
             description: 'back-end დეველოპერი'
         },
         {
-            photo: '/src/assets/images/members/member5.svg',
+            photo: '/assets/images/members/member5.svg',
             name: 'ვახო ბარათაშვილი',
             description: 'back-end დეველოპერი, UI/UX დიზაინერი'
         },
         {
-            photo: '/src/assets/images/members/member6.svg',
+            photo: '/assets/images/members/member6.svg',
             name: 'ანდრია ბიბიაშვილი',
             description: 'back-end დეველოპერი'
         }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
-import {createApp} from 'vue';
-import './assets/main.css';
+import {createApp} from 'vue'
+import '/assets/main.css'
 import App from './App.vue'
-import router from "./router";
-import {VueReCaptcha} from 'vue-recaptcha-v3';
+import router from './router'
 
 createApp(App)
-    .use(VueReCaptcha, {siteKey: import.meta.env.VITE_RECAPTCHA_SITEKEY,})
-    .use(router).mount('#app');
+    .use(router).mount('#app')
