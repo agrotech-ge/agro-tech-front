cc58b3829208c2ee27f4c4a8913c9dada3b99fe3
Gandobilis <lashadeveloper@gmail.com>
2024-03-03 02:08:41 +0400

    removed pinia.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import '/src/assets/main.css'
 import {createApp} from 'vue'
-import {createPinia} from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-const pinia = createPinia()
 const app = createApp(App)
 
-app.use(pinia)
 app.use(router)
 app.mount('#app')
